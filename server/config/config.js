2076c0288e9e122dd4c80f27d3051a76da7a6442
shizhidi <shizhido@outlook.com>
2018-02-14 22:23:48 +0800

    添加pm2配置

--- server/config/config.js
+++ server/config/config.js
@@ -2,11 +2,11 @@
  * @Author: shizhidi
  * @Date: 2018-02-01 21:42:27
  * @Last Modified by: shizhidi
- * @Last Modified time: 2018-02-03 10:20:58
+ * @Last Modified time: 2018-02-14 21:41:28
  */
 
  //需要连接的数据库,可配置多个。
 module.exports = {
   mongodb: 'mongodb://localhost:27017/db_demo',
-  mongodb2: ''
+  nodecall: 'mongodb://shizhidi:wudi114817@127.0.0.1:27017/nodecall'
 }
--- server/config/mongoose.js
+++ server/config/mongoose.js
@@ -2,15 +2,18 @@
  * @Author: shizhidi
  * @Date: 2018-02-01 21:45:00
  * @Last Modified by: shizhidi
- * @Last Modified time: 2018-02-03 10:41:15
+ * @Last Modified time: 2018-02-14 21:41:38
  */
 
 var mongoose = require('mongoose');
 var config = require('./config');
 
+var env = process.env.NODE_ENV || 'development'
+
 module.exports = function() {
     //连接数据库
-    var mongodb = mongoose.connect(config.mongodb,{useMongoClient:true});
+    var mongo = env == 'development' ? config.mongodb : config.nodecall
+    var mongodb = mongoose.connect(mongo,{useMongoClient:true});
 
     mongoose.connection.on('connected',function () {
       console.log('Mongodb connected success.');
