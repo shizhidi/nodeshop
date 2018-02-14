adb3cbde18a0397494ed4cb0da2ec2102925e4ae
shizhidi <shizhido@outlook.com>
2018-02-15 00:56:13 +0800

    更改public path

--- config/index.js
+++ config/index.js
@@ -7,7 +7,7 @@ module.exports = {
     index: path.resolve(__dirname, '../dist/index.html'),
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: 'baidu.com',
+    assetsPublicPath: '47.100.63.183',
     productionSourceMap: true,
     // Gzip off by default as many popular static hosts such as
     // Surge or Netlify already gzip all static assets for you.
