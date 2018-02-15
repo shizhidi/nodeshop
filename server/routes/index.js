77383930a7cf9e2ac466caec5470161805621dbe
shizhidi <shizhido@outlook.com>
2018-02-15 15:01:59 +0800

    dist 目录放到server

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express,Very Goood' });
+  //res.render('index', { title: 'Express,Very Goood' });
+  //const html = fs.readFileSync(path.resolve(__dirname, './dist/index.html'), 'utf-8')
+  //res.send(html)
 });
 
 module.exports = router;
